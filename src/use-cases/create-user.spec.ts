7e87348581ebbeb34b2a1f9abfa57739e25031eb
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-07-12 11:58:40 -0300

    --feat Applying unit test for user creation.

--- src/use-cases/create-user.spec.ts
+++ src/use-cases/create-user.spec.ts
@@ -1,7 +1,25 @@
-import {it, beforeAll, afterAll, describe} from 'vitest'
+import { it, beforeEach, describe, expect } from 'vitest'
 import { CreateUserUseCase } from './create-user'
 import { InMemoryUsersRepository } from './in-memory-repository/in-memory-users-repository'
 
 let usersRepository: InMemoryUsersRepository
 let sut: CreateUserUseCase
 
+describe('Create Users Unit Test', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new CreateUserUseCase(usersRepository)
+  })
+
+  it('should be able to create users', async () => {
+    const { user } = await sut.execute({
+      name: 'Lucas',
+      email: 'lucas.monteiro.13@gmail.com',
+      password: '123456',
+    })
+
+    expect(user).toMatchObject({
+      name: 'Lucas',
+    })
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,13 @@ export default defineConfig({
     ],
     dir: 'src',
     maxConcurrency: 1,
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+    ],
   },
 })
