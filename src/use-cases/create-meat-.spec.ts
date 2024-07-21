8f084034935dfb6f491b2b9e04c9485bd7d20d49
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-07-21 00:46:19 -0300

    --feat Unit test for increasing streak.

--- src/use-cases/create-meat-.spec.ts
+++ src/use-cases/create-meat-.spec.ts
@@ -2,6 +2,7 @@ import { CreateMealUseCase } from './create-meal'
 import { expect, beforeEach, it, describe } from 'vitest'
 import { InMemoryMealsRepository } from './in-memory-repository/in-memory-meals-repository'
 import { InMemoryUsersRepository } from './in-memory-repository/in-memory-users-repository'
+import { ResourceNotFoundError } from './errors/resouce-not-found-error'
 
 let userRepository: InMemoryUsersRepository
 let mealRepository: InMemoryMealsRepository
@@ -31,5 +32,43 @@ describe('Create Meat Unit Test', () => {
     expect(meal).toMatchObject({
       name: 'Pãozinho com chocolate',
     })
+  })
+
+  it('should not be able to create meal not having an id', async () => {
+    await expect(
+      sut.execute({
+        user_id: 'não existe',
+        name: 'Pãozinho com chocolate',
+        description: 'Pão com creme de chocolate maromba',
+        part_of_diet: true,
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should be able to increase streak when meal is part of diet', async () => {
+    const user = await userRepository.create({
+      email: 'lucas.monteiro@gmail.com',
+      password_hash: '123456',
+      name: 'Lucas',
+    })
+
+    await sut.execute({
+      user_id: user.id,
+      name: 'Pãozinho com chocolate',
+      description: 'Pão com creme de chocolate maromba',
+      part_of_diet: true,
+    })
+
+    await sut.execute({
+      user_id: user.id,
+      name: 'Pãozinho com chocolate',
+      description: 'Pão com creme de chocolate maromba',
+      part_of_diet: true,
+    })
+
+    const userUpdated = await userRepository.findById(user.id)
+    if (userUpdated) {
+      expect(userUpdated.current_streak).toBe(2)
+    }
   })
 })
