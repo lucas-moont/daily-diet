d899b924d6519cb62f84062d1e2404bac595005d
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-07-26 14:39:00 -0300

    --feat Adding tests for not bein able to authenticate with wrong password or email.

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it, beforeEach } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
 import { InMemoryUsersRepository } from '@/repositories/in-memory-repository/in-memory-users-repository'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 let usersRepository: InMemoryUsersRepository
 let sut: AuthenticateUseCase
@@ -28,5 +29,23 @@ describe('Authenticate Unit Tests', () => {
       name: 'Lucas',
       email: 'lucas.monteiro@email.com',
     })
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await expect(
+      sut.execute({
+        username: 'lucas.monteiro@email.com',
+        password: '12345',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with invalid email', async () => {
+    await expect(
+      sut.execute({
+        username: 'lucas.monteir@email.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
