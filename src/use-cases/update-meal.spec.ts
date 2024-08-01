d2f73eefd26e7dd7a8ab03b8b6c0a7314553cecd
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-08-01 20:33:59 -0300

    --feat Applying test for editing meals having positive implications at streaks.

--- src/use-cases/update-meal.spec.ts
+++ src/use-cases/update-meal.spec.ts
@@ -86,14 +86,59 @@ describe('Unit tests for update meals', () => {
     }
 
     await sut.execute({
-      mealId: 'ref-20',
+      mealId: 'ref-4',
       userId: user.id,
       part_of_diet: false,
     })
 
     const foundUser = await usersRepository.findById(user.id)
 
-    expect(foundUser?.longest_streak).toBe(19)
-    expect(foundUser?.current_streak).toBe(0)
+    expect(foundUser?.longest_streak).toBe(16)
+    expect(foundUser?.current_streak).toBe(16)
+  })
+
+  it('should be able to fix streaks positivily', async () => {
+    const user = await usersRepository.create({
+      email: 'lucas.monteiro@gmail.com',
+      name: 'Lucas',
+      password_hash: '123456',
+    })
+
+    await mealsRepository.create({
+      description: 'Descriçãlo qualquer',
+      name: `Refeição 1`,
+      user_id: user.id,
+      id: `ref-1`,
+      part_of_diet: true,
+    })
+
+    for (let i = 2; i < 21; i++) {
+      await mealsRepository.create({
+        description: 'Descriçãlo qualquer',
+        name: `Refeição ${i}`,
+        user_id: user.id,
+        id: `ref-${i}`,
+        part_of_diet: false,
+      })
+    }
+
+    for (let i = 2; i <= 3; i++) {
+      await sut.execute({
+        mealId: `ref-${i}`,
+        userId: user.id,
+        part_of_diet: true,
+      })
+    }
+
+    await sut.execute({
+      mealId: `ref-20`,
+      userId: user.id,
+      part_of_diet: true,
+    })
+
+    const foundUser = await usersRepository.findById(user.id)
+
+    expect(foundUser?.longest_streak).toBe(3)
+    expect(foundUser?.current_streak).toBe(1)
   })
 })
