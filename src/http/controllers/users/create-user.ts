0d9ca1e841a190473bec6387811f96f09f59a1a1
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-10-18 12:22:39 -0300

    --fix Making registration use its use case.

--- src/http/controllers/users/create-user.ts
+++ src/http/controllers/users/create-user.ts
@@ -1,4 +1,6 @@
 import { prisma } from '@/lib/prisma'
+import { EmailAlreadyRegistered } from '@/use-cases/errors/email-already-registered-error'
+import { makeCreateUserService } from '@/use-cases/factories/make-create-user-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 
@@ -14,13 +16,20 @@ export async function register(
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  await prisma.user.create({
-    data: {
+  try {
+    const createUser = await makeCreateUserService()
+    await createUser.execute({
       name,
       email,
-      password_hash: password,
-    },
-  })
+      password,
+    })
+  } catch (err) {
+    if (err instanceof EmailAlreadyRegistered) {
+      response.status(204).send({
+        message: err.message,
+      })
+    }
+  }
 
   response.status(201).send()
 }
