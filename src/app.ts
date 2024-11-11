71d3a1f53ae1c155a195142c1de9073a5efe9400
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-11-11 16:56:06 -0300

    --feat Registering fastifyjwt for app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import fastify from 'fastify'
 import { userRoutes } from './http/controllers/users/routes'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(userRoutes)
---
+++ src/http/controllers/users/authenticate.ts
@@ -0,0 +1,29 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-user'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    username: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { username, password } = authenticateBodySchema.parse(req.body)
+
+  try {
+    const authenticateUseCase = makeAuthenticateUseCase()
+    await authenticateUseCase.execute({
+      username,
+      password,
+    })
+
+    reply.status(200).send()
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      reply.status(406).send({
+        message: error.message,
+      })
+    }
+  }
+}
