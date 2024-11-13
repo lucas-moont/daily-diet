8329d05c1f5cc3b0bbc8832d2105f7ed4fe45fef
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-11-13 16:04:55 -0300

    --feat Creating utils for create and authenticate user.

---
+++ src/utils/tests/create-and-authenticate-user.ts
@@ -0,0 +1,23 @@
+import request from 'supertest'
+import { FastifyInstance } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password_hash: await hash('123456', 6),
+    },
+  })
+
+  const authResponse = await request(app.server).post('/session').send({
+    username: 'johndoe@email.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return { token }
+}
