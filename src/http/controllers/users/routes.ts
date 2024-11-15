9b8611c4bf2b0a80a49108ece8beb8e0eec4c5c9
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2024-11-15 12:07:39 -0300

    --feat Adding refresh controller.

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,10 +4,15 @@ import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { metrics } from './metrics'
+import { refresh } from './refresh'
 
 export async function userRoutes(app: FastifyInstance) {
   app.post('/sign-up', register)
   app.post('/auth', authenticate)
+
+  app.patch('/token/refresh', refresh)
+
+  /*NEEDS AUTHENTICATION*/
 
   app.get('/me', { onRequest: verifyJwt }, profile)
   app.get('/metrics', { onRequest: verifyJwt }, metrics)
